c290cf01b5ed2bec177fc507a6da9a38309d8f1b
hyun6ik <fkanwm@naver.com>
2020-10-22 10:02:49 +0900

    소코반 C++를 WinAPI로 바꿔보기

---
+++ resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Ex0915-API-Sokoban.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EX0915APISOKOBAN_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EX0915APISOKOBAN            107
+#define IDI_SMALL                       108
+#define IDC_EX0915APISOKOBAN            109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP_BLOCK                129
+#define IDB_BITMAP_BOX                  130
+#define IDB_BITMAP_HUMAN                131
+#define IDB_BITMAP_WHITE                132
+#define IDB_BITMAP1                     133
+#define IDB_BITMAP_DOT                  133
+#define IDB_BITMAP6                     138
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
